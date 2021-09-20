98cd3a30f9925aec5c02bc090f69f44b20293851
Anshu Vijay <anshuvijay2020@gmail.com>
2021-09-20 16:03:26 +0530

    Setter Injection Annotation - Autowired

--- SpringAnnotations/src/com/sample/springAnnotationDemo/TennisCoach.java
+++ SpringAnnotations/src/com/sample/springAnnotationDemo/TennisCoach.java
@@ -6,12 +6,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	private FortuneService fortuneService;
+
+
+	//define a default constructor
+	public TennisCoach() {
+		System.out.println("Tennis Coach: Inside Default Constructor.");
+	}
 	
+	//define a setter method
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println("Tennis Coach: Inside setFortuneService().");
 		fortuneService = theFortuneService;
 	}
 
+	
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Tennis Coach: Practice for 3 hours";
