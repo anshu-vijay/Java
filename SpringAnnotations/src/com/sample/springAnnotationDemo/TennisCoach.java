629d34a385ccbc8de52b4d451653ece3428c0dca
Anshu Vijay <anshuvijay2020@gmail.com>
2021-09-22 01:34:33 +0530

    Annotaion Scope Singleton to Prototype
    Here we are changing scope from singleton to prototype. Now everytime when bean is called it will creae new objects, contructor will also be called twice.

--- SpringAnnotations/src/com/sample/springAnnotationDemo/TennisCoach.java
+++ SpringAnnotations/src/com/sample/springAnnotationDemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.sample.springAnnotationDemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortune")
