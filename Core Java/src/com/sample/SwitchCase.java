1d51b198b133f3098b4627bffacd73ac7e5193db
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 01:03:33 +0530

    SwitchCase

---
+++ Core Java/src/com/sample/SwitchCase.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		int x = 2;
+		switch(x) {
+		case 1: System.out.println("one");
+		case 2: System.out.println("two");
+		case 3: System.out.println("three");
+		default: System.out.println("Byeeee....");
+		/*Output
+		  two
+		  three
+		  Byeeee....*/
+		}
+	}
+
+}
