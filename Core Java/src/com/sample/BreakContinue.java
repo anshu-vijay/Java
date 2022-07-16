ffa58634543d69a749dd2c2877b159a2f3aa4b8d
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 01:31:45 +0530

    BreakContinue
    break and continue keyword uses

---
+++ Core Java/src/com/sample/BreakContinue.java
@@ -0,0 +1,32 @@
+package com.sample;
+
+public class BreakContinue {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			if (i == 2) {
+				break;
+			}
+			/*output
+			 1
+			 2
+			 */
+		}
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			if (i == 2) {
+				continue;
+			}
+			/*output
+			 1
+			 2
+			 3
+			 4
+			 5			
+			 */
+		}
+	}
+
+}
