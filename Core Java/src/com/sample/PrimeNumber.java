d6a98e6cb54aa9c8978f78a113d19793fa7e2f86
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 02:43:47 +0530

    PrimeNumber

---
+++ Core Java/src/com/sample/PrimeNumber.java
@@ -0,0 +1,28 @@
+package com.sample;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int num = s.nextInt();
+		boolean isPrime = true;
+		for (int i = 2; i<num/2;i++) {
+			if (num%i == 0) {
+				isPrime = false;				
+				break;
+			}
+		}
+		
+		if (isPrime) {
+			System.out.println("Prime Number");			
+		}
+		else {
+			System.out.println("Not a Prime Number");
+		}
+		
+	}
+
+}
