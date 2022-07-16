35fe3c92b8ada28808cfe5d3b30225dc5b2d727a
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-16 18:04:36 +0530

    String_with_unicode_char
    Sring and Uncode character Hello World »

---
+++ Core Java/src/com/sample/String_with_unicode_char.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class String_with_unicode_char {
+
+	public static void main(String[] args) {
+		String var1 = "Hello World";
+		char var2 = '\u00BB';
+		System.out.println(var1 + " " + var2);  //Hello World »
+	}
+
+}
