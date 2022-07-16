a8adee339e7e30740bb5f0a07ff9406b00ad92ca
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 01:04:47 +0530

    SwitchCase with Break

--- Core Java/src/com/sample/SwitchCase.java
+++ Core Java/src/com/sample/SwitchCase.java
@@ -6,13 +6,15 @@ public class SwitchCase {
 		int x = 2;
 		switch(x) {
 		case 1: System.out.println("one");
+		break;
 		case 2: System.out.println("two");
+		break;
 		case 3: System.out.println("three");
+		break;
 		default: System.out.println("Byeeee....");
 		/*Output
 		  two
-		  three
-		  Byeeee....*/
+		  */
 		}
 	}
 
