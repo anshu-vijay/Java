217188bf9b27167b36b19df3f175bdc28b8eee4e
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 02:38:20 +0530

    Armstrong

---
+++ Core Java/src/com/sample/Armstrong.java
@@ -0,0 +1,28 @@
+package com.sample;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int num = s.nextInt();
+		int length = String.valueOf(num).length();
+		System.out.println("Length of number : " + length);
+		int temp = num, sum = 0, rem;
+		while (temp > 0) {
+			rem = temp % 10;
+			int remWithLengthAsPower = (int) Math.pow(rem, length);
+			sum = sum + remWithLengthAsPower;
+			temp = temp / 10;
+
+		}
+		if (sum == num) {
+			System.out.println("Armstrong");
+		} else {
+			System.out.println("Not an Armstrong");
+		}
+	}
+
+}
