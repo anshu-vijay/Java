dee7901b9dcd0bb56cc7f0f663c7e2ecb6337974
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 20:09:26 +0530

    default consructor
    when a class do no have any constructotr then it implicitly creates a default constructor

---
+++ OOPS/src/com/oopsconcepts/SmartPhone_main.java
@@ -0,0 +1,11 @@
+package com.oopsconcepts;
+
+public class SmartPhone_main {
+
+	public static void main(String[] args) {
+		SmartPhone_pojo mobile = new SmartPhone_pojo();
+		System.out.println(mobile.getBrand());
+	}
+	//output
+	//Samsung
+}
---
+++ OOPS/src/com/oopsconcepts/SmartPhone_pojo.java
@@ -0,0 +1,13 @@
+package com.oopsconcepts;
+
+public class SmartPhone_pojo {
+	private String brand = "Samsung";
+
+	public SmartPhone_pojo() {
+		
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+}
