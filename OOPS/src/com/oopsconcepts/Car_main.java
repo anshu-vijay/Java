1c801b52b9a6b9eaeadbd1da07d3c1dd6ca2bc16
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-17 19:52:32 +0530

    constructor

--- OOPS/src/com/oopsconcepts/Car_main.java
+++ OOPS/src/com/oopsconcepts/Car_main.java
@@ -4,16 +4,19 @@ public class Car_main {
 
 	public static void main(String[] args) {
 		Car_pojo car = new Car_pojo();
+		car.run();  //using constructor values where no property has been defined
 		car.setDoors("Closed");
 		car.setDriver("Seated");
 		System.out.println("Tyres : " + car.getTyres());
 		System.out.println("Driver : " + car.getDriver());
 		System.out.println("Doors : " + car.getDoors());
-		car.run();
+		car.run();	//using setter values 
 	}
 	/*
 	 * output 
-	 * Tyres : 4 
-	 * Driver : Sitted
-	 * Doors : Closed
+	 	Car is not running
+		Tyres : 4
+		Driver : Seated
+		Doors : Closed
+		Car is running
 	 */}
--- OOPS/src/com/oopsconcepts/Car_pojo.java
+++ OOPS/src/com/oopsconcepts/Car_pojo.java
@@ -1,32 +1,42 @@
 package com.oopsconcepts;
 
 public class Car_pojo {
-private final int tyres = 4;
-private String driver;
-private String doors;
-
-public String getDriver() {
-	return driver;
-}
-public void setDriver(String driver) {
-	this.driver = driver;
-}
-public String getDoors() {
-	return doors;
-}
-public void setDoors(String doors) {
-	this.doors = doors;
-}
-public int getTyres() {
-	return tyres;
-}
-
-public void run() {
-	if (doors.equals("Closed") && driver.equals("Seated")) {
-		System.out.println("Car is running");
+	private final int tyres = 4;
+	private String driver;
+	private String doors;
+	
+	public Car_pojo() {
+		super();
+		this.driver = "absent";
+		this.doors = "opened";
 	}
-	else {
-		System.out.println("Car is not running");
+
+	public String getDriver() {
+		return driver;
 	}
-}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public String getDoors() {
+		return doors;
+	}
+
+	public void setDoors(String doors) {
+		this.doors = doors;
+	}
+
+
+	public int getTyres() {
+		return tyres;
+	}
+
+	public void run() {
+		if (doors.equals("Closed") && driver.equals("Seated")) {
+			System.out.println("Car is running");
+		} else {
+			System.out.println("Car is not running");
+		}
+	}
 }
