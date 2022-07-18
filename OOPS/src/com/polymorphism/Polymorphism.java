a0c1e05ddc19d4407966b9a27536d6440b590be9
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-18 14:55:52 +0530

    polymmorphism - method overloading
    same method name, different parameter data type

--- OOPS/src/com/polymorphism/Polymorphism.java
+++ OOPS/src/com/polymorphism/Polymorphism.java
@@ -5,6 +5,9 @@ public class Polymorphism {
 	public static void main(String[] args) {
 		System.out.println(Adder.add(11, 11)); // 22
 		System.out.println(Adder.add(11, 11, 11)); // 33
+		
+		System.out.println(Subtracter.subtract(11,5)); //6 
+		System.out.println(Subtracter.subtract(12.3,2.6));  //9.700000000000001
 
 		Vehicle v = new Vehicle();
 		v.run();// Bike is running safely
---
+++ OOPS/src/com/polymorphism/Subtracter.java
@@ -0,0 +1,11 @@
+package com.polymorphism;
+
+public class Subtracter {
+	static double subtract(double a, double b) {
+		return a - b;
+	}
+
+	static int subtract(int a, int b) {
+		return a - b;
+	}
+}
