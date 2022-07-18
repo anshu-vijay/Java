d7c19e909406e1f4ee0a9be4c344aa02929081ba
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-18 14:39:08 +0530

    polymorphism - method overloading

---
+++ OOPS/src/com/polymorphism/Polymorphism.java
@@ -0,0 +1,10 @@
+package com.polymorphism;
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		System.out.println(Adder.add(11,11));  //22
+		System.out.println(Adder.add(11,11,11));  //33
+	}
+
+}
