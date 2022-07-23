9e00e68a121d78e13d568838f61cafce999a1623
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-23 15:08:07 +0530

    Multiple Try Catch blocks

---
+++ Exception Handling/src/com/exceptionHandling/MultipleCatchBlocks.java
@@ -0,0 +1,15 @@
+package com.exceptionHandling;
+
+public class MultipleCatchBlocks {
+
+	public static void main(String[] args) {
+		try {
+			int x = 10 / 0;
+			System.out.println("This statement will not get executed."); // This statement will not get executed.
+//		} catch (ArithmeticException e) {
+//			System.out.println(e); // java.lang.ArithmeticException: / by zero
+		} catch (Exception e) {
+			System.out.println(e); // java.lang.ArithmeticException: / by zero
+		}
+	}
+}
