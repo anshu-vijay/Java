10909a902fdf063e14a717c4c80c6217b14747ae
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-24 19:07:28 +0530

    list iterator
    it contains previos and next both

--- Collections Framework/src/com/sample/iterator/Solution.java
+++ Collections Framework/src/com/sample/iterator/Solution.java
@@ -2,6 +2,7 @@ package com.sample.iterator;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Solution {
 
@@ -15,6 +16,11 @@ public class Solution {
 		Iterator<String> itr = carsList.iterator();
 		while(itr.hasNext()) {
 			System.out.println(itr.next());
+		}
+		
+		ListIterator<String> itr2 = carsList.listIterator();
+		while(itr2.hasNext()) {
+			System.out.println(itr2.next());
 		}
 	}
 
