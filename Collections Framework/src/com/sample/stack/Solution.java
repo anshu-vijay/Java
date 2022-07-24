f7599818e1d252e8ca675ecd0cc7d9a7cc55f7c1
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-24 18:18:22 +0530

    Stack

---
+++ Collections Framework/src/com/sample/stack/Solution.java
@@ -0,0 +1,22 @@
+package com.sample.stack;
+
+import java.util.Stack;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Stack<Integer> stackObj =new Stack<Integer>();
+		stackObj.push(20);
+		stackObj.push(50);
+		stackObj.push(70);
+		stackObj.push(30);
+		System.out.println(stackObj);  	//[20, 50, 70, 30]
+		stackObj.pop();		
+		System.out.println(stackObj); 	//[20, 50, 70]
+		stackObj.push(90);
+		System.out.println(stackObj); 	//[20, 50, 70, 90]
+		
+		System.out.println("peek: " + stackObj.peek()); 	//peek: 90
+	}
+
+}
