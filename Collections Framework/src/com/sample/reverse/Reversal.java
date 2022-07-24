0ae9047bb63eb4dc4237d03ff75569d86deebb4a
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-24 19:14:23 +0530

    Reversal list1

--- Collections Framework/src/com/sample/reverse/Reversal.java
+++ Collections Framework/src/com/sample/reverse/Reversal.java
@@ -1,9 +1,23 @@
 package com.sample.reverse;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Reversal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<String> carsList = new ArrayList<String>();
+		carsList.add("Volvo");
+		carsList.add("BMW");
+		carsList.add("Ford");
+		carsList.add("Mazda");
+		
+		Collections.reverse(carsList);
+		System.out.println(carsList);
+		
+		//[Mazda, Ford, BMW, Volvo]
 
 	}
 
