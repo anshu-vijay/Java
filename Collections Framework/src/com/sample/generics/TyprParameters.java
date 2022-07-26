f0b06f1279e85eb5edf06f09c610a872379d95d1
Anshu Vijay <90796405+anshu-vijay@users.noreply.github.com>
2022-07-27 00:04:26 +0530

    type parameter generics

---
+++ Collections Framework/src/com/sample/generics/TyprParameters.java
@@ -0,0 +1,41 @@
+package com.sample.generics;
+
+class Example<K, V> {
+
+	private K key;
+
+	private V value;
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public Example(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Example [key=" + key + ", value=" + value + "]";
+	}
+
+	public <E, N> void display(E element, N number) {
+		System.out.println("Element= " + element + " Number= " + number);
+	}
+
+}
+
+public class TyprParameters {
+
+	public static void main(String[] args) {
+		Example<Integer, String> obj = new Example(1, "Anshu");
+		System.out.println(obj);
+		obj.display("Element", 105);
+	}
+
+}
